12c4051734a3e03cacb2fe3e13a5e7c6388c851f
Oscar Ruiz <ogruizr@gmail.com>
2023-09-28 20:41:15 -0500

    Se agrega el nuevo mensaje

--- app.mjs
+++ app.mjs
@@ -3,7 +3,7 @@ import { createServer } from 'http';
 const server = createServer((request, response) => {
 response.writeHead(200, { 'content-type': 'text/plain; charset=utf-8' });
 response.write('Hello ');
-response.end(' World\n');
+response.end(' World, My name is Oscar Ruiz\n');
 });
 
 server.listen(8080, () => {
